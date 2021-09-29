f37b804ecbfd4b50ca3f37092767cfabfa9ba5c9
Rsekkat <rayan.sekkat@gmail.com>
2021-09-29 15:00:01 +0200

    test

--- usb/src/App.vue
+++ usb/src/App.vue
@@ -1,8 +1,28 @@
 <template>
-  <div id="app" class="app">
-    <a href="#" id="display" @click="connect_usb"> Connection to usb device </a>
-    <p id="display"></p>
-    <HelloWorld name="test"></HelloWorld>
+  <div class="app">
+    <label for="vendorId">Vendor ID: 0x</label>
+    <input
+      type="text"
+      name="vendorId"
+      placeholder="vendorId"
+      id="vendor"
+      value="2a03"
+    />
+    <p>
+      <a href="#" id="click" @click="click">Click me to get USB device info</a>
+    </p>
+    <!-- <div v-for="test in devices" :key="test.length" class="helloworld">
+      <HelloWorld
+        v-for="helloworld in device"
+        :key="helloworld.productId"
+        :name="helloworld.productName"
+      ></HelloWorld>
+    </div> a approfondir -->
+    <HelloWorld
+      v-for="helloworld in devices"
+      :key="helloworld.productId"
+      :name="helloworld.productName"
+    ></HelloWorld>
   </div>
 </template>
 <script>
@@ -14,12 +34,30 @@ export default {
   components: {
     HelloWorld,
   },
+  data() {
+    return {
+      devices: 0,
+      device: "",
+    };
+  },
   methods: {
-    connect_usb: function () {
-      // PROBLEME AVEC CETTE TECHNIQUE C'EST QU'IL FAUT RECCUPÉRER LE VENDORID MAIS IL DOIT Y AVOIR UNE AUTRE SOLUTION
+    click: function () {
+      var vendorId = "0x" + document.getElementById("vendor").value;
+      navigator.usb
+        .requestDevice({ filters: [{ vendorId: vendorId }] })
+        .then((device) => {
+          console.log(device);
+          console.log("Product ID: " + device.productId.toString(16));
+          console.log("Vendor ID: " + device.vendorId.toString(16));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
       navigator.usb.getDevices().then((devices) => {
+        this.devices = devices.length;
         console.log("Total devices: " + devices.length);
         devices.forEach((device) => {
+          this.device = device.productName;
           console.log(
             "Product name: " +
               device.productName +
@@ -28,13 +66,6 @@ export default {
           );
         });
       });
-    },
-    test: function () {
-      var usb = require("usb");
-      document.getElementById("display").innerHTML = JSON.stringify(
-        usb.getDeviceList()
-      );
-      console.log(usb.getDeviceList());
     },
   },
 };
@@ -58,5 +89,8 @@ console.log(OS);
 .app {
   display: flex;
   justify-content: center;
+}
+
+.hellorworld {
 }
 </style>
